d8c4a64b4647855bdcd9c3901126da8ab280175d
Juan Felipe Torres <jf.torresp@outlook.com>
2020-09-23 11:24:58 -0500

    Initial Commit

---
+++ Somnia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Somnia
+//
+//  Created by Juan Felipe Torres on 23/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
