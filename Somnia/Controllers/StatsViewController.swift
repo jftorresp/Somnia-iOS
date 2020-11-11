a01bc660ed133fac4f42a8dd5e1d2638bdd9e13b
Juan Felipe Torres <jf.torresp@outlook.com>
2020-11-11 11:59:19 -0500

    Fetch analysis

--- Somnia/Controllers/StatsViewController.swift
+++ Somnia/Controllers/StatsViewController.swift
@@ -37,10 +37,11 @@ class StatsViewController: UIViewController {
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Analysis: \(SleepTwoViewController.analysis)")
+        
+        loadAnalysis()
         
         dailyAnalysisView.layer.cornerRadius = 12
         deepView.layer.cornerRadius = 12
@@ -49,44 +50,46 @@ class StatsViewController: UIViewController {
         awakeView.layer.cornerRadius = 12
         snoreView.layer.cornerRadius = 12
         breathView.layer.cornerRadius = 12
-
+        
     }
     
     func loadAnalysis() {
         
-        db.collection(K.FStore.analysisCollection)
-            .getDocuments { (querySnapshot, error) in
-                
-                SleepActivitiesViewController.sleepStories = []
-                
-                if let e = error {
-                    print("There was an issue retrieving data from Firestore. \(e)")
-                } else {
-                    for document in querySnapshot!.documents {
-                        
-                        let data = document.data()
-                        
-                        let createdBy = data["createdBy"] as? String
-                        let deep = data["deep"] as? Int
-                        let duration = data["duration"] as? Double
-                        let hourStage = data["hourStage"] as? Int
-                        let image = data["image"] as? String
-                        
-                        if let nameR = name, let fileN = fileName, let descrip = description, let dur = duration, let img = image {
+        if let id = Auth.auth().currentUser?.uid {
+            
+            db.collection(K.FStore.analysisCollection)
+                .whereField(K.FStore.createdByField, isEqualTo: id)
+                .getDocuments { (querySnapshot, error) in
+                    
+                    SleepActivitiesViewController.sleepStories = []
+                    
+                    if let e = error {
+                        print("There was an issue retrieving data from Firestore. \(e)")
+                    } else {
+                        for document in querySnapshot!.documents {
                             
-                            let newStory = Stories(n: nameR, f: fileN, img: img, d: descrip, dur: dur, a: nil)
+                            let data = document.data()
                             
-                            SleepActivitiesViewController.sleepStories.append(newStory)
+                            let deep = data["deep"] as? Double
+                            let duration = data["duration"] as? Double
+                            let hourStage = data["hourStage"] as? [String: String]
+                            let light = data["light"] as? Double
+                            let nightDate = data["nightDate"] as? Date
+                            let rem = data["rem"] as? Double
+                            let snorePer = data["snorePercentage"] as? Double
+                            let totalEvents = data["totalEvents"] as? Int
+                            let totalSnores = data["totalSnores"] as? Int
+                            let wake = data["wake"] as? Double
                             
-                            DispatchQueue.main.async {
-                                self.storiesTableView.reloadData()
-                                let indexPath = IndexPath(row: SleepActivitiesViewController.sleepStories.count - 1, section: 0)
-                                self.storiesTableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                            if let d = deep, let dur = duration, let hour = hourStage, let l = light, let n = nightDate, let r = rem, let per = snorePer, let events = totalEvents, let snores = totalSnores, let w = wake {
+                                
+                                let analysis = Analysis(nightDate: n, duration: dur, totalEvents: events, totalSnores: snores, snorePercentage: per, wake: w, light: l, deep: d, rem: r, hourStage: hour)
+                                print("Analysis: \(analysis)")
                             }
                         }
                     }
                 }
-            }
+        }
     }
-
+    
 }
